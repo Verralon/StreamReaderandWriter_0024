27cd995be74261570cac93c4534f828c3b32c50c
Verralon <indradiana056@gmail.com>
2026-06-01 13:21:30 +0700

    kondisi jika throw yang bertipe data integer

--- ManualException.cpp
+++ ManualException.cpp
@@ -10,3 +10,8 @@ int main()
         //throw 0.5; //melemparkan sebuah integer maka
         cout << "Pernyataan tidak akan dtampilkan" << endl;
     }
+    //kondisi jika throw yang bertipe data integer
+    catch (int e)
+    {
+        cout << "Pengecualian akan dieksekusi"<< endl;
+    }
