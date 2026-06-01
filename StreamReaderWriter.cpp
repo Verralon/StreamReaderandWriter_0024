4496a7d6ceedf099fb25e06663242f3d1093d222
Verralon <indradiana056@gmail.com>
2026-06-01 13:29:26 +0700

    Menambahkan penutupan file setelah penulisan dan membuka file untuk membaca

--- StreamReaderWriter.cpp
+++ StreamReaderWriter.cpp
@@ -28,3 +28,12 @@ int main()
         outfile << baris << endl;
 
     }
+    //menutup file setelah penulisan selesai
+    outfile.close();
+
+    //membuka file dalam mode membaca
+    ifstream infile;
+    //menunjuk ke sebuah nama file
+    infile.open("contohfile.txt");
+
+    cout << endl << ">=Membuka dan membaca file<=" << endl;
